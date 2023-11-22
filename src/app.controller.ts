a6cf83b1ed6176418aa4afa7ee501d26001a5667
Tadesse Alemayehu <tadessedev@gmail.com>
2023-11-22 16:19:11 +0300

    subscribe to a multiply service and return the multiplayed result

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @MessagePattern('multiply')
+  multiplyNumber(data: number[]) {
+    console.log(`multiplying number with ${data[0]} and ${data[1]}`);
+    return this.appService.multiplyNumber(data);
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
-@Injectable()
+
 export class AppService {
+  multiplyNumber(data: number[]) {
+    console.log(`multiplying number with ${data[0]} and ${data[1]}`)
+    return data[0] * data[1];
+  };
   getHello(): string {
     return 'Hello World!';
   }
